388a9e33567ae9a876c915d719e5f597c74a1fd2
Minene <55697285+Minene314@users.noreply.github.com>
2023-11-22 00:51:37 +0900

    Add files via upload

---
+++ search-directory/test2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void print() {
+    return;
+}
+
+int calc(int a, int b) {
+    return 0;
+}
+
+int main() {
+    int n = 0;
+    cout << "test" << endl;
+    return 0;
+}
